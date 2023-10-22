c0e70c3c41261d78ed688252e2149be8fde1ce38
Praveena0989 <praveena.poojary@cleartax.in>
2023-10-23 00:56:03 +0530

    Remove bgcolor

--- MarkupEditor/LinkViewController.swift
+++ MarkupEditor/LinkViewController.swift
@@ -88,7 +88,6 @@ class LinkViewController: UIViewController {
         view.addSubview(buttonStack)
         removeButton = UIButton(configuration: .borderedProminent(), primaryAction: nil)
         removeButton.preferredBehavioralStyle = UIBehavioralStyle.pad
-        removeButton.configuration?.baseBackgroundColor = view.backgroundColor
         removeButton.configuration?.title = "Remove Link"
         // Avoid word wrapping
         removeButton.configuration?.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
